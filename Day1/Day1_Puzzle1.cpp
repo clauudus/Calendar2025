cdabd0dfde914066402e09d20e7ad5736da454a6
clauudus <96309298+clauudus@users.noreply.github.com>
2025-12-02 22:13:13 +0100

    Add files Day 1
    Files added:
    Code first puzzle
    Input text file

---
+++ Day1/Day1_Puzzle1.cpp
@@ -0,0 +1,42 @@
+// Type your code here, or load an example.
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ifstream input("input.txt");
+
+    if(!input)
+    {
+        cout << "This is the first day and I'm already failing wtf" << endl;
+        return 0;    
+    }
+
+    int pos = 50; //initial position
+    int count0 = 0 //times we go though 0
+
+    char dir; //direction we will rotate
+    int steps; //how many steps we will rotate
+
+    while (input >> dir >> steps)
+    {
+        if(dir == 'L') //I'm only gonna check L because I know the text file only goes with uppercase, could be changed for an or though
+        {
+            pos -= steps;
+            pos %= 100;
+            if(pos < 0) pos += 100;
+        } else if (dir == 'R') { //same here, but with another text file could go with 'r'
+            pos = (pos + steps) % 100;
+        } else {
+            continue; //i'm not gonna check much, this is just for sanity check, but I know this input file is complete
+        }
+
+        if (pos == 0) ++count0;
+    }
+
+    cout << count0;
+
+    return 0;
+}
